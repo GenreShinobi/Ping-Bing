27005eb42d525cf486dd13dea5c04c0c6712d2f2
genreshinobi <genreshinobi@gmail.com>
2022-07-07 11:51:35 -0600

    Add paddle movement and boundary collision
    The ball will change angle depending on the section of the paddle it collides with and will now bounce off the top and bottom boundaries

--- game/ball/ball.gd
+++ game/ball/ball.gd
@@ -16,7 +16,29 @@ func _physics_process(delta :float):
 		
 		if collision:
 			if collision.collider.is_in_group("paddle"):
-				speed = -speed
+				speed = speed * Vector2(-1, 1)
+				print(collision.collider.get_groups())
+				if collision.collider.is_in_group("paddle_top"):
+					speed.y = -400.0
+				elif collision.collider.is_in_group("paddle_bot"):
+					speed.y = 400.0
+				elif collision.collider.is_in_group("paddle_mid"):
+					speed.y = 0.0
+			if collision.collider.is_in_group("boundary"):
+				speed = speed * Vector2(1, -1)
+				
+
+func _process(delta :float):
+	var fps = Engine.get_frames_per_second()
+	var lerp_interval = speed / fps
+	var lerp_position = global_transform.origin + lerp_interval
+	
+	if fps > 60:
+		set_as_toplevel(true)
+		global_transform.origin = global_transform.origin.linear_interpolate(lerp_position, 20 * delta)
+	else:
+		global_transform = global_transform
+		set_as_toplevel(false)
 
 func setStartingPosition() -> void:
 	position = starting_position
--- game/paddle/paddle.gd
+++ game/paddle/paddle.gd
@@ -3,6 +3,11 @@ extends Node2D
 # padding
 var padding := 16.0
 
+# speed
+var paddle_speed = Vector2(0.0, 400.0)
+
+onready var panel = $PanelContainer
+
 # paddle side
 export (bool) var right_paddle :bool = false
 
@@ -11,6 +16,14 @@ func _ready() -> void:
 	if is_in_group("ai_paddle"):
 		setAIPaddleSide()
 	setStartingPosition()
+
+
+func _physics_process(delta :float):
+	if is_in_group("player_paddle"):
+		if(Input.is_key_pressed(KEY_W)):
+			position += -paddle_speed * delta
+		if(Input.is_key_pressed(KEY_S)):
+			position += paddle_speed * delta
 
 
 func setStartingPosition() -> void:
