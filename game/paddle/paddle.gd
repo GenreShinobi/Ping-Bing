735b72f09d233e74e3dd4e6ea2ec45a8c9c62057
genreshinobi <genreshinobi@gmail.com>
2022-07-07 12:05:13 -0600

    Add clamping on the top and bottom for paddle movement

--- game/paddle/paddle.gd
+++ game/paddle/paddle.gd
@@ -2,11 +2,11 @@ extends Node2D
 
 # padding
 var padding := 16.0
-
-# speed
 var paddle_speed = Vector2(0.0, 400.0)
+var paddle_size = Vector2(16, 100)
 
 onready var panel = $PanelContainer
+var ball
 
 # paddle side
 export (bool) var right_paddle :bool = false
@@ -16,6 +16,8 @@ func _ready() -> void:
 	if is_in_group("ai_paddle"):
 		setAIPaddleSide()
 	setStartingPosition()
+	#TODO: Update to compensate for multiple balls
+	ball = get_tree().get_nodes_in_group("ball")[0]
 
 
 func _physics_process(delta :float):
@@ -24,6 +26,10 @@ func _physics_process(delta :float):
 			position += -paddle_speed * delta
 		if(Input.is_key_pressed(KEY_S)):
 			position += paddle_speed * delta
+	if is_in_group("ai_paddle"):
+		position.y = ball.position.y
+		
+	position.y = clamp(position.y, 0.0 + (paddle_size.y/2) + 4, Screen.SCREEN_HEIGHT - (paddle_size.y / 2) - 4)
 
 
 func setStartingPosition() -> void:
